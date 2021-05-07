f30274c44d69453d0fc26c3c1ceb5d2e891ee3b4
matt <matte@fastmail.com>
2021-05-07 13:44:46 -0500

    first push

---
+++ README.md
@@ -0,0 +1,93 @@
+# Just A Modal
+
+Sometimes you just need a simple modal, to either get a confirmation before deleting something or to present some info to a user. This is one. You won't get 500 npm modules 
+added to your project with this.
+
+* zero dependencies
+* overlay automatically added to dim the background
+* small
+* easy to get started with and use
+
+## Installation
+```
+npm install --save just-a-modal
+```
+
+## Usage
+```jsx
+import JustAModal from 'just-a-modal';
+
+const [modalInfo, setModalInfo] = useState({
+    showModal: false,
+    actionID: '',
+    action: '',
+    config: {}
+});
+
+const handleDelete = (id) => {
+    let modalInfo = {
+        showModal: true,
+        actionID: id,
+        action: 'deleteBook',
+        config: {}
+    };
+
+    setModalInfo(modalInfo);
+};
+
+const handleCancel = () => {
+    let modalInfo = {
+        showModal: false,
+        actionID: '',
+        action: '',
+        config: {}
+    };
+
+    setModalInfo(modalInfo);
+};
+
+const handleAction = (actionID) => {
+    if(modalInfo.action === 'deleteBook'){
+        //api call to delete book with id of actionID
+        //after it completes, close the modal
+        handleCancel();
+    }
+};
+
+<JustAModal modalInfo={modalInfo} handleCancel={handleCancel} handleAction={handleAction} />
+
+```
+
+with an actionID set:
+<br />
+![alt text](https://github.com/meberhardt2/just-a-modal/blob/main/screenshots/confirmation.png?raw=true)
+
+set actionID to 0 and modal will change from a delete confirmation to a info box:
+<br />
+![alt text](https://github.com/meberhardt2/just-a-modal/blob/main/screenshots/info.png?raw=true)
+
+
+|modalInfo||
+|---|---|
+|showModal|boolean true to show the modal, false to hide it|
+|actionID|id of item to delete. if missing, modal will be be an info modal|
+|action|action that you want to perform if user clicks ok|
+|config|object, modal config options|
+<br />
+
+
+|config||
+|---|---|
+|title|default for confirmation is "Alert", info is "Info"|
+|body|default for confirmation is "Are you sure?", info needs a body|
+|buttonOKText|default is "OK"|
+|buttonDeleteText|default is "Delete"|
+|buttonCancelText|default is "Cancel"|
+<br />
+
+```
+config:{
+    title: 'Alert!!',
+    buttonDeleteText: 'I am super sure'
+}
+```
---
+++ src/just-a-modal.js
@@ -0,0 +1,116 @@
+import React, { Fragment, useState, useEffect } from 'react';
+
+import 'just-a-modal/just-a-modal.css';
+
+/**************************************************************************************/
+function Modal(props){
+
+    /****************************************/
+    const [config, setConfig] = useState({
+        modal: 'modalJAM fade',
+        cover: 'modal-backdropJAM fade',
+        title: '',
+        body: '',
+        type: '',
+        buttonCancelText: '',
+        buttonOKText: '',
+        buttonDeleteText: ''
+    });
+	/****************************************/
+
+
+	/****************************************/
+    useEffect(() => {
+        modalConfig(props);
+    }, [props]);
+	/****************************************/
+
+
+	/****************************************/
+    const modalConfig = (props) => {
+        if(typeof props.handleCancel !== 'function'){
+            console.log('a valid cancel function was not given');
+        }
+        if(typeof props.handleAction !== 'function'){
+            console.log('a valid action function was not given');
+        }
+
+        let newConfig = {};
+        if(props.modalInfo.showModal){
+            newConfig.modal = 'modalJAM fade show block';
+            newConfig.cover = 'modal-backdropJAM fade show';
+
+            if(typeof props.modalInfo.actionID === 'undefined' || props.modalInfo.actionID === '' || props.modalInfo.actionID === 0){
+                newConfig.type = 'info';
+                if(typeof props.modalInfo.config === 'undefined'){
+                    newConfig.title = 'Info';
+                    newConfig.body = 'I need my body set';
+                    newConfig.buttonOKText = 'OK';
+                }
+                else{
+                    newConfig.title = typeof props.modalInfo.config.title !== 'undefined' && props.modalInfo.config.title !== '' ? props.modalInfo.config.title : 'Info';
+                    newConfig.body = typeof props.modalInfo.config.body !== 'undefined' && props.modalInfo.config.body !== '' ? props.modalInfo.config.body : 'I need a body';
+                    newConfig.buttonOKText = typeof props.modalInfo.config.buttonOKText !== 'undefined' && props.modalInfo.config.buttonOKText !== '' ? props.modalInfo.config.buttonOKText : 'OK';
+                }
+            }
+            else{
+                newConfig.type = 'delete';
+                newConfig.actionID = props.modalInfo.actionID;
+                if(typeof props.modalInfo.config === 'undefined'){
+                    newConfig.title = 'Alert';
+                    newConfig.body = 'Are you sure?';
+                    newConfig.buttonCancelText = 'Cancel';
+                    newConfig.buttonDeleteText = 'Delete';
+                }
+                else{
+                    newConfig.title = typeof props.modalInfo.config.title !== 'undefined' && props.modalInfo.config.title !== '' ? props.modalInfo.config.title : 'Alert';
+                    newConfig.body = typeof props.modalInfo.config.body !== 'undefined' && props.modalInfo.config.body !== '' ? props.modalInfo.config.body : 'Are you sure?';
+                    newConfig.buttonCancelText = typeof props.modalInfo.config.buttonCancelText !== 'undefined' && props.modalInfo.config.buttonCancelText !== '' ? props.modalInfo.config.buttonCancelText : 'Cancel';
+                    newConfig.buttonDeleteText = typeof props.modalInfo.config.buttonDeleteText !== 'undefined' && props.modalInfo.config.buttonDeleteText !== '' ? props.modalInfo.config.buttonDeleteText : 'Delete';
+                }
+            }
+
+        }
+        else{
+            newConfig.modal = 'modalJAM fade';
+            newConfig.cover = 'modal-backdropJAM fade';
+        }
+
+        setConfig(newConfig);
+    }
+	/****************************************/
+
+
+    /****************************************/
+	return(
+        <Fragment>
+            <div className={config.modal} role="dialog">
+                <div className="modal-dialogJAM" role="document">
+                    <div className="modal-contentJAM">
+                        <div className="modal-headerJAM">
+                            <h5 className="modal-titleJAM">{config.title}</h5>
+                        </div>
+                        <div className="modal-bodyJAM">{config.body}</div>
+                        <div className="modal-footerJAM">
+                            {config.type === 'delete' &&
+                                <Fragment>
+                                    <button type="button" className="btnJAM btn-dangerJAM" onClick={() => {props.handleAction(config.actionID)}}>{config.buttonDeleteText}</button>
+                                    <button type="button" className="btnJAM btn-secondaryJAM" onClick={props.handleCancel}>{config.buttonCancelText}</button>
+                                </Fragment>
+                            }
+
+                            {config.type !== 'delete' &&
+                                <button type="button" className="btnJAM btn-primaryJAM" onClick={props.handleCancel}>{config.buttonOKText}</button>
+                            }
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={config.cover}></div>
+        </Fragment>
+    );
+	/****************************************/
+}
+/**************************************************************************************/
+
+export default Modal;
